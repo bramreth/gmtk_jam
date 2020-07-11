b07788a5c26538d241cde1e20beb90fca476237f
Sam Le-Cornu <slecornu96@gmail.com>
2020-07-11 16:57:15 +0100

    Moved stipulation logic to super tile to allow any tile have this functionality

--- proj/scenes/action_tiles/action_tile.gd
+++ proj/scenes/action_tiles/action_tile.gd
@@ -1,8 +1,15 @@
 extends Area2D
 
+export (GameManager.stipulation) var stipulation = GameManager.stipulation.NONE
+
 func _on_body_entered(body):
 	if body.name == "player":
 		_on_player_entered(body)
+		_on_stipulation()
 
 func _on_player_entered(player):
 	pass
+
+func _on_stipulation():
+	GameManager.stipulation(stipulation)
+	queue_free()
--- proj/scenes/action_tiles/dialog_tile.gd
+++ proj/scenes/action_tiles/dialog_tile.gd
@@ -1,15 +1,6 @@
 extends "res://scenes/action_tiles/action_tile.gd"
 
-export(int) var tile_action = 0
-
 export (Dialog.Sequence) var dialog_sequence
 
 func _on_player_entered(player):
-	print(player.name)
 	EventBus.emit_signal("start_dungeon_master_dialog", Dialog.Sequence.keys()[dialog_sequence])
-	match tile_action:
-		1:
-			GameManager.stipulation(GameManager.stipulation.FAST)
-		_:
-			pass
-	queue_free()
