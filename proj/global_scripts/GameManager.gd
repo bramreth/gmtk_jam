949f738c84bb81d141c5e4457dfd886e1e5a289b
Sam Le-Cornu <slecornu96@gmail.com>
2020-07-12 16:40:37 +0100

    bg music

--- proj/global_scripts/GameManager.gd
+++ proj/global_scripts/GameManager.gd
@@ -10,12 +10,24 @@ var dialog_json
 var hud_active = false
 var player = null
 
+
+onready var bg_music := AudioStreamPlayer.new()
+
 func _ready() -> void:
 	var dialog_data = File.new()
 	if dialog_data.file_exists(dialog_path): 
 		dialog_data.open(dialog_path, dialog_data.READ)
 		dialog_json = JSON.parse(dialog_data.get_as_text()).result
 		dialog_data.close()
+	
+	add_child(bg_music)
+	bg_music.stream = load("res://sounds/bg2.wav")
+	bg_music.autoplay = true
+	bg_music.connect("finished", self, "_play_music")
+	_play_music()
+	
+func _play_music():
+	bg_music.play()
 
 func get_dialog(name):
 	 return dialog_json[name]
