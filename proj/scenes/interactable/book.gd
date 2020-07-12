2ce2929832a631bc25c805c9afb777cfa4b36435
Sam Le-Cornu <slecornu96@gmail.com>
2020-07-12 18:12:21 +0100

    page sound

--- proj/scenes/interactable/book.gd
+++ proj/scenes/interactable/book.gd
@@ -2,4 +2,5 @@ extends "res://scenes/interactable/interactable.gd"
 
 func interact(verb):
 	if verb == Verbs.READ or verb == Verbs.OPEN:
+		_play_sound("sfx/page.wav")
 		EventBus.emit_signal("show_material", id)
