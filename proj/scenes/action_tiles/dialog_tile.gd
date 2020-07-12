e23429b0dafba897f795982253cfe447a1eb886d
Sam Le-Cornu <slecornu96@gmail.com>
2020-07-12 13:28:55 +0100

    dialog tile is invisible

--- proj/scenes/action_tiles/dialog_tile.gd
+++ proj/scenes/action_tiles/dialog_tile.gd
@@ -2,6 +2,9 @@ extends "res://scenes/action_tiles/action_tile.gd"
 
 export (Dialog.Sequence) var dialog_sequence
 
+func _ready():
+	sprite.visible = false
+	
 func _on_player_entered(player):
 	Dialog.start(dialog_sequence)
 
